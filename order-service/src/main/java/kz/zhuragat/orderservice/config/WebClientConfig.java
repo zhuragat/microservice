3442e013921f4da82bdbc92669aecd719c1a5ac8
Zhuragat <zhuragat.bolatkhan@gmail.com>
2023-05-21 11:47:34 +0600

    added discovery server

--- order-service/src/main/java/kz/zhuragat/orderservice/config/WebClientConfig.java
+++ order-service/src/main/java/kz/zhuragat/orderservice/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package kz.zhuragat.orderservice.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.*;
 import org.springframework.web.reactive.function.client.*;
 
@@ -7,7 +8,8 @@ import org.springframework.web.reactive.function.client.*;
 public class WebClientConfig {
 
     @Bean
-    public WebClient webClient() {
-        return WebClient.builder().build();
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
     }
 }
--- order-service/src/main/java/kz/zhuragat/orderservice/service/OrderService.java
+++ order-service/src/main/java/kz/zhuragat/orderservice/service/OrderService.java
@@ -19,7 +19,7 @@ import java.util.*;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
     public void placeOrder(OrderRequest orderRequest)  {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -29,8 +29,8 @@ public class OrderService {
         List<String> skuCodes = items.stream().map(OrderLineItem::getSkuCode)
                 .toList();
         // call Inventory Service and place order if product is in stock
-        InventoryResponse[] inventoryResponseArray = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
